da0699fc5cf6a68b5d6e03cdd78553c66efef4cf
Juan Francisco Rojas Suarez <jfspideyrs@gmail.com>
2026-03-10 22:51:37 -0500

    return_types.py

--- return_types.py
+++ return_types.py
@@ -1,6 +1,5 @@
 def obtener_precio_usuario():
-    """Función para obtener el precio del usuario y convertirlo a un número"""
-    preciostr = input("Ingrese el precio del producto:\n")
-    return float(preciostr)
+    entrada = input("Ingrese el precio del producto: ")
+    return float(entrada)
 precio = obtener_precio_usuario()
 print(precio)
