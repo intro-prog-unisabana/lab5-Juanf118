f3db540f971ba18de61f6f592ca1ca353406e2a0
Juan Francisco Rojas Suarez <jfspideyrs@gmail.com>
2026-03-12 10:29:26 -0500

    function_types.py

--- function_types.py
+++
@@ -1,8 +0,0 @@
-def list_shift(lst, value):
-    """Modificar una lista sumando un valor a cada elemento."""
-    entrada = float (lst[0])
-    for i in range(len(lst)):
-        lst[i] += value
-    return (lst)
-print(list_shift([1, 2, 3], 5))
-
