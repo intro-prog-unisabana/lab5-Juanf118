fd5b579febae6071bec5aa15b82ff5f40292305d
Juan Francisco Rojas Suarez <jfspideyrs@gmail.com>
2026-03-14 22:20:23 -0500

    scope.py

--- scope.py
+++ scope.py
@@ -6,7 +6,7 @@ def set_globals(some_int, some_str):
     global str_global
     int_global = some_int
     str_global = some_str
-set_globals(10, "Hola")
+set_globals(10, "Hola") == int_global == 10 and str_global == "Hola"
 
 def get_globals():
     return (int_global, str_global)
