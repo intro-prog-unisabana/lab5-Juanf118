c0b4dd92f8242511b499a85884809c407a8f284a
Juan Francisco Rojas Suarez <jfspideyrs@gmail.com>
2026-03-14 22:36:16 -0500

    random_q.py

---
+++ random_q.py
@@ -0,0 +1,8 @@
+import random
+random.seed(123)
+
+inicio = int(input("Enter the start value: \n"))
+fin = int(input("Enter the end value: \n"))
+int_aleatorio = random.randint(inicio, fin)
+print(f"Generated random number: {int_aleatorio}")
+
