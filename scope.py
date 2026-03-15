fee26f95ec18195b016c8aa72b1fe9ab2f566633
Juan Francisco Rojas Suarez <jfspideyrs@gmail.com>
2026-03-14 22:24:39 -0500

    scope.py

--- scope.py
+++ scope.py
@@ -2,11 +2,9 @@ int_global = None
 str_global = None
 
 def set_globals(some_int, some_str):
-    global int_global
-    global str_global
+    global int_global, str_global
     int_global = some_int
     str_global = some_str
-set_globals(10, "Hola") == int_global == 10 and str_global == "Hola"
 
 def get_globals():
     return (int_global, str_global)
