b0bd3a66f9fd5f9eec66bb13f5b17812116ce030
Juan Francisco Rojas Suarez <jfspideyrs@gmail.com>
2026-03-15 17:39:25 -0500

    random_q.py

--- random_q.py
+++ random_q.py
@@ -5,4 +5,3 @@ inicio = int(input("Enter the start value: \n"))
 fin = int(input("Enter the end value: \n"))
 int_aleatorio = random.randint(inicio, fin)
 print(f"Generated random number: {int_aleatorio}")
-
